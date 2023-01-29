8186a8195af06e2f907ff051e782e74a8e16fc86
Akshat Tiwari <takshat0811@gmail.com>
2023-01-29 20:29:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function displayTime() {
+    var date = new Date();
+    var y = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var d = date.getDate();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var ampm = 'AM';
+    if (h ==0) {
+        h = 12;
+    }
+    if (h>12) {
+        h = h - 12;
+        ampm = 'PM';
+    }
+    h = (h<10) ? '0'+h:h;
+    m = (m<10) ? "0"+m:m;
+    s = (s<10)? "0"+s:s;
+
+
+    var time = y + "/" + month + "/" + d + " " + h + ":" + m + ":" + s+'' + ampm;
+    document.getElementById('clockdisplay').innerHTML = time;
+    document.getElementById("clockdisplay").textContent = time;
+    setTimeout(displayTime, 1000);
+}
+displayTime();
